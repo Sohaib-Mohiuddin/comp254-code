6e4c5c2d5f6938220b91e966d8bffcefed12b529
Sohaib <sohaibm131@gmail.com>
2025-05-15 13:10:19 -0400

    Updated

--- Labs/Lab1Exercises/Lab1Exercise.java
+++
@@ -1,4 +0,0 @@
-package com.opsdevelop.teaching.Labs.Lab1Exercises;
-
-public class Lab1Exercise {
-}
--- Labs/Lab1Exercises/UsingGenerics/Pair.java
+++
@@ -1,7 +0,0 @@
-package com.opsdevelop.teaching.Labs.Lab1Exercises.UsingGenerics;
-
-//generic interface
-public interface Pair<K, V> {
-    public K getKey();
-    public V getValue();
-}
